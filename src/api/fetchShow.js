0962f8f5c7ff110c92c69b5007e5999221448aa8
Nathan Cowley <nathancowley12@gmail.com>
2020-07-20 15:25:34 -0600

    This is rediculous...but mvp

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -4,7 +4,7 @@ const fetchShow = () => {
   return axios.get("https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes")
   .then(res => {
     console.log(res)
-    return res
+    return res.data
   })
   .catch(err => {
     console.log('there was an error fetching data', err)
--- src/components/Episodes.test.js
+++ src/components/Episodes.test.js
@@ -1,33 +1,20 @@
+import React from 'react'
+import { render } from '@testing-library/react'
 
+import Episodes from './Episodes'
 
-
-
-
-
-
-
-// import React from 'react'
-// import Episodes from './Episodes'
-// import { render } from '@testing-library/react'
-
-
-// // test('Episodes.js is rendering', () => {
-// //     const { getByTestId } = render(<Episodes />);
-// //     getByTestId(`episodes`);
-// // })
-
-// test('Episodes render correctly with or without props', () => {
-//     const mockData = {
-//         id: '123',
-//         image: { medium: 'medium_image'},
-//         name: 'name',
-//         season: 3,
-//         number: 2,
-//         summary: '<p>Summary</p>',
-//         runtime: 20
-//     }
-//     const { queryAllByText, rerender } = render(<Episodes episodes={[]}/>);
-//     expect(queryAllByText(/season/i)).toHaveLength(0);
-//     rerender(<Episodes episodes={[mockData]}/>);
-//     expect(queryAllByText(/name/i)).toHaveLength(1)
-// })
+test('Renders Episodes without props, and again with props', () => {
+    const mockData = {
+        id: '123',
+        image: { medium: 'medium_image'},
+        name: 'name',
+        season: 3,
+        number: 2,
+        summary: '<p>Summary</p>',
+        runtime: 20
+    }
+    const { queryAllByText, rerender } = render(<Episodes episodes={[]}/>)
+    expect(queryAllByText(/season/i)).toHaveLength(0)
+    rerender(<Episodes episodes={[mockData]}/>)
+    expect(queryAllByText(/name/i)).toHaveLength(1)
+})
